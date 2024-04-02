668050ed84955861aaeed8a7c100e371b64937a7
Omi <121783352+salunkheom@users.noreply.github.com>
2024-04-02 22:08:04 +0530

    Update script.js
    del udate

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ arr.forEach(button =>{
                 input.value = string;
             }
             else if (e.target.innerHTML == 'DEL') {
-                string = string.substring(0, stringlength - 1);
+                string = string.substring(0, string.length - 1);
                 input.value = string;
             }
             else{
